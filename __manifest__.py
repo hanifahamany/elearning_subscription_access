11fcadf00e0c0cc4e285a1665d0ad4a48a0e4948
unknown <hnfhamany@gmail.com>
2025-06-13 11:06:46 +0700

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'eLearning Subscription Access Control',
+    'version': '1.0',
+    'category': 'eLearning',
+    'author': 'PT. Lintang Utama Infotek',
+    'summary': 'Restrict eLearning course access based on subscription status',
+    'depends': ['website_slides', 'sale_subscription', 'sale'],
+    'data': [
+    #   "views/website_slides_templates_course_inherit.xml",
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
